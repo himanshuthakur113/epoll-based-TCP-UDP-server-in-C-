d997c94ffbc6ed7b1311e49366d0de94bd82110d
Himanshu Thakur <himanshu97687@gmail.com>
2025-08-29 20:47:46 +0530

    done basics

--- src/client.cpp
+++ src/client.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <sys/socket.h> //for socket system call
+#include <netinet/in.h> // for address strucute
+#include <string.h>     //for strlen
+#include <arpa/inet.h>  //for inet_pton converting it string into ip address
+#include <unistd.h>     //for close()
 
 const int server_port = 12960;
 const char* server_ip = "127.0.0.1"; //pc has multiple ip address for connecting with iteself it used a loopback address 
@@ -37,7 +40,26 @@ int main(){
 
     //connect intiates a tcp handshake to establish a connection SYN,SYN_ACK,ACK
 
+    std::cout << "Connected to the server" <<std::endl;
+
+    char message[] = {"Hello from client"};
+
+
+    send(client_socket,message,strlen(message),0);
     
+    //this send the message to kernal buffer associated with this socket 
+    //after buffere passes to the date to kernal tcp/ip stack 
+    //where in transport layer the host and destination port number are assigned to data packets with sequence no.(only in tcp)
+    //they are converted it either into segment or datagram
+    //in the next internet layer ip address header are assigned 
+    //in the link layer combination of physical or data link the mac address is assigned 
+    //and the kernal pass the data to the nic buffer here nic driver come into use 
+    //this passing from kernal to nic is done by dma without cpu intervention
+    
+    close();
+
+    return 0;
+}
     
 
 
--- src/server.cpp
+++ src/server.cpp
@@ -1,13 +1,16 @@
+
 #include <iostream>
-#include <sys/socket.h> //used for socket system call
-#include <netinet/in.h> // used for storing address
+#include <sys/socket.h> //for socket system call
+#include <netinet/in.h> // for address strucute
+#include <string.h>     //for strlen
+#include <arpa/inet.h>  //for inet_pton converting it string into ip address
+#include <unistd.h>     //for close()
 
 const int port = 12960;
 
 int main(){
    
-
-    int server_socket = socket(AF_INET,SOCK_STREAM,0); //this creates a socket in kernal & return a file descriptor for process decriptor table 
+    
                                                 
     if(server_socket == -1)
     {
@@ -15,7 +18,9 @@ int main(){
         return -1;
     }
 
-    sockaddr_in server_address;   //datatype for holding address of server
+    sockaddr_in server_address; //datatype for holding address of server
+
+    int address_lenght = sizeof(server_address);
 
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(port);  //port are logical endpoint  that is used to direct network traffic to specific process or sevices
@@ -23,7 +28,9 @@ int main(){
 
     server_address.sin_addr.s_addr = INADDR_ANY; // s_addr is part of sin_addr which store the ip address the actual value is stored in s_addr
                                                  //a pc can have multiple ip address INADDR_ANY means this socket can accept connection from any of them 
+
     
+
    if(bind(server_socket, (struct sockaddr*)(&server_address),sizeof(server_address)) < 0) //bind can work with sockaddr_in and sockaddr_in6 to this bind need a pointer to sockaddr
    {                                                                                       //which contain both sockaddr_in 4 & 6 we need to cast the address into sockaddr pointer
         std::cerr << "Binding failed" <<std::endl;
@@ -31,11 +38,30 @@ int main(){
    }
 
   
-    listen(server_socket,5) //so now the socket is waiting for client to connect
+    if(listen(server_socket,5) < 0)
+    {
+        std::cerr << "listen failure" <<std::endl;
+    }
+                                  //so now the socket is waiting for client to connect
                             //5 is backlog the max no. of pending request the os should hold for this socket these request has completed a 3 way handshake
-    std::cout << "Listening for connection" <<std::endl;
+    std::cout << "Listening for connection" << port <<std::endl;
+ 
+    
+    communication_socket = accept(server_socket,(struct sockaddr*)&server_address,(socklen_t)&address_lenth);  //created a new socket which take its data from old socket since it is server this process will handle multiple clients so one socket form connecting to client and other for communication 
+                                                                                                                          //we used the accept call here and the third parameter we passed is pointer to struct socklen_t which is size of server address before intialization telling the accept what would be size and after accept modify it to actual size
 
+    if(communication_socket < 0)
+    {
+        std::cerr << "communication socket failed" <<std::endl;
+    }
 
+    std::cout << "connection accepted from client" <<std::endl;
+
+    char buffer[1024] = {0};
+
+    recv(communication_socket,buffer,1024,0);
+
+    std::cout << "client buffer" << buffer << std::endl;
 
 
     return 0;
